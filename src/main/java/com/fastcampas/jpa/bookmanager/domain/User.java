4f19c767c843c2f7a28204e2c415d7f8c159352d
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-08 21:38:51 +0900

    user entity

--- src/main/java/com/fastcampas/jpa/bookmanager/domain/User.java
+++ src/main/java/com/fastcampas/jpa/bookmanager/domain/User.java
@@ -2,19 +2,29 @@ package com.fastcampas.jpa.bookmanager.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
     @NonNull
     private String name;
     @NonNull
     private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 
     // jpa 도메인은 기본적으로 언제 생성/수정되었는지 저장이 된다.
     // 그걸 관리 하기 위해 다음 필드를 만들어둔다.

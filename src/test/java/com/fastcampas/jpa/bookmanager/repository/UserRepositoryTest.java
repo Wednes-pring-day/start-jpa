d91e97bb85c8521524813454451efded836112e6
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-14 03:39:51 +0900

    count(), existsById()

--- src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,14 +1,8 @@
 package com.fastcampas.jpa.bookmanager.repository;
 
-import com.fastcampas.jpa.bookmanager.domain.User;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest // 스프링 컨텍스트를 불러와서 테스트에 활용하겠다는 의미! 테스트의 주체는 Junit(Jupiter이므로.)
 class UserRepositoryTest {
@@ -17,12 +11,12 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        // flush()는 어떻게 동작하는지 콘솔에서 확인하기 힘들다.
-        // 이후에 영속성 컨텍스트에 대해 공부할 때 자세히 알아보자!
-        userRepository.save(new User("new kayoung", "newkayoung@wednespring.com"));
-        userRepository.flush();
+        long count = userRepository.count();
+        System.out.println(count); // 5
 
-        userRepository.saveAndFlush(new User("rosie", "rosie@wednespring.com"));
-        userRepository.findAll().forEach(System.out::println);
+        boolean exists = userRepository.existsById(1L); //  count 쿼리로 구현되어있다. 값이 1인지 아닌지로 구분.
+
+        System.out.println(exists);
+
     }
 }

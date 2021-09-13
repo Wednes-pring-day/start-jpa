d3f49c04aedc9c20c4222ab8898c055089cdcfeb
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-14 04:28:45 +0900

    queryByExample: examplematcher with ignorePaths

--- src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -4,8 +4,12 @@ import com.fastcampas.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 @SpringBootTest // 스프링 컨텍스트를 불러와서 테스트에 활용하겠다는 의미! 테스트의 주체는 Junit(Jupiter이므로.)
 class UserRepositoryTest {
@@ -14,15 +18,10 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        Page<User> users = userRepository.findAll(PageRequest.of(1, 3));
-
-        System.out.println("page: " + users);
-        System.out.println("totalElements: " + users.getTotalElements()); // 한 페이지에 들어가는 엘리멘트의 개수
-        System.out.println("totalPages: " + users.getTotalPages()); // 결과의 페이지의 개수
-        System.out.println("numberOfElements: " + users.getNumberOfElements());
-        System.out.println("sort: " + users.getSort());
-        System.out.println("size: " + users.getSize());
-
-        users.getContent().forEach(System.out::println);
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("name")
+                .withMatcher("email", endsWith());
+        Example<User> example = Example.of(new User("probe", "google.com"), matcher);
+        userRepository.findAll(example).forEach(System.out::println);
     }
 }

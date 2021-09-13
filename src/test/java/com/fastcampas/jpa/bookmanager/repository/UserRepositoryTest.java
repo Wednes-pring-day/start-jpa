e206f47b920c75c9efa0d7b432f99d96f2e6a874
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-14 04:21:30 +0900

    jpa paging

--- src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.fastcampas.jpa.bookmanager.repository;
 
+import com.fastcampas.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @SpringBootTest // 스프링 컨텍스트를 불러와서 테스트에 활용하겠다는 의미! 테스트의 주체는 Junit(Jupiter이므로.)
 class UserRepositoryTest {
@@ -11,7 +14,15 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.deleteAll(); // findAll - 각 row 마다 delete 쿼리 : 총 다섯번
-        userRepository.findAll().forEach(System.out::println);
+        Page<User> users = userRepository.findAll(PageRequest.of(1, 3));
+
+        System.out.println("page: " + users);
+        System.out.println("totalElements: " + users.getTotalElements()); // 한 페이지에 들어가는 엘리멘트의 개수
+        System.out.println("totalPages: " + users.getTotalPages()); // 결과의 페이지의 개수
+        System.out.println("numberOfElements: " + users.getNumberOfElements());
+        System.out.println("sort: " + users.getSort());
+        System.out.println("size: " + users.getSize());
+
+        users.getContent().forEach(System.out::println);
     }
 }

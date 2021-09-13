4247631d9bcf8d71141082dd212f828ab92370e0
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-14 02:49:36 +0900

    test: find all user sorted by name

--- src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampas/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -4,6 +4,9 @@ import com.fastcampas.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootTest // 스프링 컨텍스트를 불러와서 테스트에 활용하겠다는 의미! 테스트의 주체는 Junit(Jupiter이므로.)
 class UserRepositoryTest {
@@ -12,7 +15,7 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.save(new User());
-        userRepository.findAll().forEach(System.out::println);
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name")); // 이름 역순으로 조회
+        users.forEach(System.out::println);
     }
 }
